ff6fd78cb306be43b241eba85da3094fbb1f9a62
Tanmay <tanmaydixit33@gmail.com>
2023-08-11 15:54:53 +0530

    changes for warning

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar(props) {
   return (
     <>
    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand mx-2" href="#">Video Player</a>
+  <a class="navbar-brand mx-2" href="/#">Video Player</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
